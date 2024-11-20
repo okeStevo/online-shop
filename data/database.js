e68cc7c65c2564512320d3df0b6c94d82a568b8f
stevo <stephenoke2007@gmail.com>
2024-11-20 22:50:05 +0100

    finished version of the minimal onlineshop built with html, css, js and node.js

---
+++ data/database.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+
+let database;
+
+async function connectToDatabase() {
+  const client = await MongoClient.connect(
+    process.env.MONGO_URL || "mongodb://127.0.0.1:27017"
+  );
+  database = client.db("online-shop");
+}
+
+function getDb() {
+  if (!database) {
+    throw new Error("You must connect first!");
+  }
+
+  return database;
+}
+
+module.exports = {
+  connectToDatabase: connectToDatabase,
+  getDb: getDb,
+};
